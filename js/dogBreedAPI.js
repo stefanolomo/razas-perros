15a50cacc1310839b93236d9ba2135a8c285875a
stefanolomo <stefanolomo@gmail.com>
2023-01-02 05:53:39 -0300

    Dog breed API

---
+++ js/dogBreedAPI.js
@@ -0,0 +1,31 @@
+let BreedListApiUrl = "https://dog.ceo/api/breeds/list/all";
+let BaseUrlApi = "https://dog.ceo/api/breed/"
+let BreedDOMContainer = document.querySelector(".breed-container")
+let BreedImageContainer = document.querySelector(".breed-image-container")
+let ImageDisplay = document.querySelector(".img")
+
+fetch(BreedListApiUrl)
+	.then((response) => response.json())
+	.then((data) => {
+		for (const breed in data.message) {
+			let CapitalizedBreed = breed.replace(/^\w/, c => c.toUpperCase())
+			const HtmlTemplate = `<option value="${breed}">${CapitalizedBreed}</option>`
+			BreedDOMContainer.insertAdjacentHTML('beforeend', HtmlTemplate)
+		}
+	})
+
+let consumeApi = (Breed) => {
+	fetch(`${BaseUrlApi}${Breed}/images`)
+	.then((response) => response.json())
+	.then((data) => {
+		data.message.slice(0, 15).forEach(BreedImage => {
+			const HtmlTemplate = `<img class="img" src="${BreedImage}" alt="${Breed}">`
+			BreedImageContainer.insertAdjacentHTML('beforeend', HtmlTemplate)
+		});
+	})
+}
+
+let getBreed = (element) => {
+	let Breed = element.value
+	consumeApi(Breed)
+};
